e952dc6c90573498bda25ff7fd8b53ca7074965e
James Putnam <putnamjm.design@gmail.com>
2022-07-17 16:46:09 -0400

    #4 - merge libraries into preface

--- dist/eko.mk
+++ dist/eko.mk
@@ -3,52 +3,39 @@
 #
 SRC = ../src
 
-COMMON = \
-	common.l   \
-	describe.l \
-        print.l
-
-COMPILE = \
-	compile.l
-
-SYSTEM = \
-	system.l   \
-        environment.l
-
-METRICS = \
-	metrics.l
-
 CORE = \
-	core.l	     \
-        compile.l    \
-        eval.l	     \
-        exceptions.l \
-        functions.l  \
-        format.l     \
-        lambda.l     \
-        lists.l      \
-        load.l       \
-        macro.l      \
-        quasiquote.l \
-        read.l       \
-        sequences.l  \
-        streams.l    \
+	core.l	     	\
+        compile.l    	\
+        eval.l	     	\
+        exceptions.l 	\
+        functions.l  	\
+        format.l     	\
+        lambda.l     	\
+        lists.l      	\
+        load.l       	\
+        macro.l      	\
+        quasiquote.l 	\
+        read.l       	\
+        sequences.l  	\
+        streams.l    	\
         strings.l
 
 PREFACE = \
-	preface.l   \
-	lists.l	    \
-	repl.l	    \
-	require.l   \
-	state.l	    \
+	preface.l   	\
+	common.l   	\
+	compile.l	\
+	describe.l 	\
+	lists.l	    	\
+	metrics.l	\
+	repl.l	    	\
+	require.l   	\
+	state.l	    	\
+        environment.l	\
+        print.l
 
 eko:
-	@(cd $(SRC)/common ; cat $(COMMON)) > eko:common.l
-	@(cd $(SRC)/compile ; cat $(COMPILE)) > eko:compile.l
 	@(cd $(SRC)/core ; cat $(CORE)) > eko:core.l
-	@(cd $(SRC)/metrics ; cat $(METRICS)) > eko:metrics.l
 	@(cd $(SRC)/preface; cat $(PREFACE)) > eko:preface.l
-	@(cd $(SRC)/system ; cat $(SYSTEM)) > eko:system.l
 
 clean:
-	@rm -f eko:common.l eko:core.l eko:frame.l eko:preface.l eko:repl.l eko:system.l eko:compile.l
+	@rm -f eko:core.l eko:preface.l
--- perf/preface-test.sh
+++ perf/preface-test.sh
@@ -3,5 +3,4 @@
     -l ./eko/eko/eko:core.l	       	                          \
     -q '(core:load "./eko/eko/eko:preface.l" () ())'             \
     -q "(core:eval (core:compile '(defconst preface::require-path \"./eko/lib-modules\")))" \
-    -q "(core:eval (core:compile '(preface:require-lib metrics)))"  \
     -q "(core:eval (core:compile '$*))"
